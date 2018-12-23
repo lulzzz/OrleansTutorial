25af93280308e2eb020b00a77273857726696aba
Chen Yu Pao <windperson@gmail.com>
2018-12-24 07:24:47 +0800

    Use ASP.NET Core generic host as silo host

---
+++ NetCoreGenericHost/HostedServices/OrleansSiloHostedService.cs
@@ -0,0 +1,89 @@
+﻿using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Kritner.OrleansGettingStarted.GrainInterfaces;
+using Kritner.OrleansGettingStarted.Grains;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Serilog;
+
+namespace NetCoreGenericHost.HostedServices
+{
+    public class OrleansSiloHostedService : IHostedService
+    {
+        private readonly IApplicationLifetime _applicationLifetime;
+        private readonly ILogger<OrleansSiloHostedService> _logger;
+        private ISiloHost _siloHost;
+
+        public OrleansSiloHostedService(IApplicationLifetime applicationLifetime, ILogger<OrleansSiloHostedService> logger)
+        {
+            _applicationLifetime = applicationLifetime;
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("register application life time");
+            _applicationLifetime.ApplicationStarted.Register(OnApplicationStartedAsync);
+            _applicationLifetime.ApplicationStopping.Register(OnApplicationStopping);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            //do nothing
+            return Task.CompletedTask;
+        }
+
+        private async void OnApplicationStartedAsync()
+        {
+            _logger.LogInformation("initialize Orleans silo host...");
+
+            _siloHost = CreateSiloHost();
+           await _siloHost.StartAsync();
+        }
+
+        private async void OnApplicationStopping()
+        {
+            _logger.LogInformation("stopping Orleans silo host...");
+            await _siloHost.StopAsync();
+        }
+
+        private static ISiloHost CreateSiloHost()
+        {
+            // define the cluster configuration
+            var builder = new SiloHostBuilder()
+                .UseDashboard(options => { options.Port = 8099; })
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "HelloWorldApp";
+                })
+                .ConfigureServices(services =>
+                {
+                    services
+                        .AddLogging(loggingBuilder => loggingBuilder.AddSerilog())
+                        .AddTransient<VisitTracker>();
+                })
+                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(VisitTracker).Assembly).WithReferences())
+                .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
+                //.ConfigureLogging(logging =>
+                //{
+                //    logging.AddConsole();
+                //    logging.AddDebug();
+                //})
+                ;
+
+            builder.AddMemoryGrainStorageAsDefault();
+
+            var host = builder.Build();
+            return host;
+        }
+    }
+}
---
+++ NetCoreGenericHost/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace NetCoreGenericHost
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var logConfig = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Orleans.Runtime.Management.ManagementGrain", LogEventLevel.Warning)
+                .MinimumLevel.Override("Orleans.Runtime.SiloControl", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Trace()
+                .WriteTo.Debug();
+
+            Log.Logger = logConfig
+                .Enrich.FromLogContext().CreateLogger();
+
+            try
+            {
+                var genericHost = CreateHostBuilder(args).Build();
+                await genericHost.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "generic host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            new HostBuilder()
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("hostsettings.json", optional: true)
+                        .AddEnvironmentVariables(prefix: "ORLEANS_HOST_")
+                        .AddCommandLine(args);
+                })
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.AddJsonFile("appsettings.json", optional: true)
+                        .AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                            optional: true)
+                        .AddEnvironmentVariables(prefix: "ORLEANS_HOST_APP_")
+                        .AddCommandLine(args);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.Configure<HostOptions>(option =>
+                    {
+                        option.ShutdownTimeout = System.TimeSpan.FromSeconds(20);
+                    });
+
+                    services.AddHostedService<HostedServices.OrleansSiloHostedService>();
+                })
+                .UseConsoleLifetime()
+                .UseSerilog();
+
+    }
+}
